cca8936ac041910d765cc542b4d3fd84a44397dd
Srirangan <srirangan@gmail.com>
2012-08-03 17:32:21 +0530

    Basic top level api tests

---
+++ test/GooglePlacesTest.js
@@ -0,0 +1,41 @@
+var assert = new require("assert");
+
+var config = require("./config.js");
+
+var GooglePlaces = require("../index.js");
+var googlePlaces = new GooglePlaces(config.apiKey, config.outputFormat);
+var parameters;
+
+/**
+ * Place search - https://developers.google.com/places/documentation/#PlaceSearchRequests
+ */
+parameters = {
+  location:[-33.8670522, 151.1957362],
+  types:"doctor"
+};
+googlePlaces.placeSearch(parameters, function (response) {
+  assert.notEqual(response.results.length, 0, "Place search must not return 0 results");
+});
+
+/**
+ * Text search - https://developers.google.com/places/documentation/#TextSearchRequests
+ */
+parameters = {
+  query:"restaurants in dublin"
+};
+googlePlaces.textSearch(parameters, function (response) {
+  assert.notEqual(response.results.length, 0, "Text search must not return 0 results");
+});
+
+/**
+ * Place details requests - https://developers.google.com/places/documentation/#PlaceDetails
+ */
+parameters = {
+  location:[-33.8670522, 151.1957362],
+  types:"doctor"
+};
+googlePlaces.placeSearch(parameters, function (response) {
+  googlePlaces.placeDetailsRequest({reference:response.results[0].reference}, function (response) {
+    assert.equal(response.status, "OK", "Place details request response status is OK");
+  });
+});
