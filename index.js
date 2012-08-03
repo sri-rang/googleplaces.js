1e6af7d378cfb69c52ca6a6c5e473d9b2a8f07cc
Srirangan <srirangan@gmail.com>
2012-08-03 17:28:34 +0530

    "module.exports" not "exports"

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var PlaceSearch = require("./lib/PlaceSearch.js");
 var TextSearch = require("./lib/TextSearch.js");
 var PlaceDetailsRequest = require("./lib/PlaceDetailsRequest.js");
 
-exports = function (apiKey, outputFormat) {
+module.exports = function (apiKey, outputFormat) {
   return {
     placeSearch:new PlaceSearch(apiKey, outputFormat),
     textSearch:new TextSearch(apiKey, outputFormat),
