bab72584024b28dd11690e8717c9faa4c571ebef
Sri <srirangan@gmail.com>
2018-07-04 19:30:45 +0200

    dump : staged changes

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 (function () {
     "use strict";
 
-    var PlaceSearch = require("./lib/PlaceSearch.js");
-    var RadarSearch = require("./lib/RadarSearch.js");
-    var TextSearch = require("./lib/TextSearch.js");
-    var PlaceDetailsRequest = require("./lib/PlaceDetailsRequest.js");
-    var PlaceAutocomplete = require("./lib/PlaceAutocomplete.js");
-    var AddEvent = require("./lib/AddEvent.js");
-    var DeleteEvent = require("./lib/DeleteEvent.js");
-    var EventDetails = require("./lib/EventDetails.js");
-    var ImageFetch = require("./lib/ImageFetch.js");
-    var NearBySearch = require('./lib/NearBySearch.js');
+    const PlaceSearch = require("./lib/PlaceSearch.js");
+    const RadarSearch = require("./lib/RadarSearch.js");
+    const TextSearch = require("./lib/TextSearch.js");
+    const PlaceDetailsRequest = require("./lib/PlaceDetailsRequest.js");
+    const PlaceAutocomplete = require("./lib/PlaceAutocomplete.js");
+    const AddEvent = require("./lib/AddEvent.js");
+    const DeleteEvent = require("./lib/DeleteEvent.js");
+    const EventDetails = require("./lib/EventDetails.js");
+    const ImageFetch = require("./lib/ImageFetch.js");
+    const NearBySearch = require('./lib/NearBySearch.js');
 
     module.exports = function (apiKey, outputFormat) {
         return {
@@ -24,7 +24,6 @@
             eventDetails: new EventDetails(apiKey, outputFormat),
             imageFetch: new ImageFetch(apiKey),
             nearBySearch: new NearBySearch(apiKey, outputFormat)
-
         };
     };
 
